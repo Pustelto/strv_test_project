1d44868510e25d463f175132db93711d9a18e461
Tomas Pustelnik <xpustelnikt@seznam.cz>
2015-01-18 23:35:46 +0100

    mail validation and sing-up functionality

--- js/posli.js
+++ js/posli.js
@@ -1,3 +1,122 @@
 $(function(){
+	var input = $('input'),
+		button = $('button'),
+		form = $('form'),
+		timeoutID;		
+
+	/*Select text in input field on focus*/
+	input.on('focus', function(){
+		$(this).select();		
+	})
+		.on('focusout', function(){
+			if( !$(this).val() ) {
+				$(this).removeClass('error');
+			}
+		});
+
+	/*Fallback for placeholder text in input field for browser that don't support this attribute*/
+	function elementSupportsAttribute(element, attribute) {
+		var test = document.createElement(element);
+		if (attribute in test) {
+			return true;
+		} else {
+			return false;
+		}
+	};
+
+	if( !elementSupportsAttribute('input', 'placeholder') ) {	
+		var	that;
+		
+		input.val('Input your Email for Beta');
+		input.on('focus', function(){
+			that = $(this);
+		})
+			.on('focusout', function(){
+				if( !that.val() ) {
+					that.val('Input your Email for Beta');
+				}
+			});
+	}
+
+	/*Validate email function*/
+	function isEmailValid(email){
+		var mail = email;
+
+		if((/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i.test(email))){
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	function validateEmail(mail, origin){
+
+		if( isEmailValid(mail) ) {			
+			origin.removeClass('error')
+			return true;
+		} else {	
+			origin.addClass('error');
+			return false;
+		}
+	}
+
+	input.on('keyup', function(){
+		var $this = $(this);			
+
+		if( $this.val().length > 3 ){
+			clearTimeout(timeoutID);
+			timeoutID = setTimeout(function(){
+				validateEmail($this.val(), $this)
+			}, 1000);
+		} else {
+			$this.removeClass('error');
+		}
+	});
 	
+	/*Send email*/	
+	form.on('submit', function(e){
+		var emailInput = $(this).children('input'),
+			email = emailInput.val(),
+			$this = $(this),
+			appendElem = '<div class="share"><a class="button facebook" href="http://www.facebook.com">Share on Facebook</a>'
+				+ '<a class="button twitter" href="http://www.twitter.com">Share on Twitter</a>'
+				+ '<p class="form-info">You\'ve signed up, we love you for that! Tell your friends about it.</p></div>'
+	
+		if( validateEmail(email, emailInput) ) {
+			$.post('#', emailInput.serialize(), function(){	
+				$this.after(appendElem);			
+			$this.hide();
+			});
+		}		
+		e.preventDefault();	
+	});
+
+	/*Inform about Ajax errors*/
+	$( document ).ajaxError(function() {
+	  form.after( '<p class="form-info">Failed to sent the email.</p>' );
+	});
+	
+	/*Smooth scrolling from menu to section*/
+	$(function() {
+		$('a[href*=#]:not([href=#])').click(function() {
+			if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+				var target = $(this.hash);
+				target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+				if (target.length) {
+					$('html,body').animate({
+						scrollTop: target.offset().top
+					}, 700);
+					return false;
+				}
+			}
+		});
+	});
+
+	//???
+	//Označit vybraný odkaz, sekci na stránce
+		//kliknutím na odkaz v menu zůstane odkaz podtrhnutý
+		//pokud jsem v sekci, která je v menu, tato sekce se podtrhne
+		/*při načtení stránky si uložím pozici sekcí, při změně velikosti okna je přeuložím, Při scrollu kontroluji, zda jsem se dotkl nějaké hodnoty, pokud ano
+			tak jí označím v menu.
+		*/
 });
