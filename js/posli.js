35200a343922f296a71172acb372d92c1a209bd4
Tomas Pustelnik <xpustelnikt@seznam.cz>
2015-01-20 01:44:17 +0100

    File control and clean up

--- js/posli.js
+++ js/posli.js
@@ -4,16 +4,6 @@ $(function(){
 		form = $('form'),
 		timeoutID;		
 
-	/*Select text in input field on focus*/
-	input.on('focus', function(){
-		$(this).select();		
-	})
-		.on('focusout', function(){
-			if( !$(this).val() ) {
-				$(this).removeClass('error');
-			}
-		});
-
 	/*Fallback for placeholder text in input field for browser that don't support this attribute*/
 	function elementSupportsAttribute(element, attribute) {
 		var test = document.createElement(element);
@@ -38,26 +28,40 @@ $(function(){
 			});
 	}
 
+	/*Select text in input field on focus*/
+	input.on('focus', function(){
+		$(this).select();		
+	})
+		.on('focusout', function(){
+			if( !$(this).val() ) {
+				$(this).removeClass('error');
+			}
+	});
+
 	/*Validate email function*/
-	function isEmailValid(email){
-		var mail = email;
-
-		if((/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i.test(email))){
-			return true;
-		} else {
-			return false;
-		}
-	}
-
 	function validateEmail(mail, origin){
+		
+		function isEmailValid(email){
+			var mail = email;
 
-		if( isEmailValid(mail) ) {			
-			origin.removeClass('error')
-			return true;
-		} else {	
-			origin.addClass('error');
-			return false;
+			if((/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i.test(email))){
+				return true;
+			} else {
+				return false;
+			}
 		}
+
+		function validate(mail, origin){
+			if( isEmailValid(mail) ) {			
+				origin.removeClass('error')
+				return true;
+			} else {	
+				origin.addClass('error');
+				return false;
+			}
+		}
+
+		return validate(mail, origin);
 	}
 
 	input.on('keyup', function(){
@@ -80,7 +84,8 @@ $(function(){
 			$this = $(this),
 			appendElem = '<div class="share"><a class="button facebook" href="http://www.facebook.com">Share on Facebook</a>'
 				+ '<a class="button twitter" href="http://www.twitter.com">Share on Twitter</a>'
-				+ '<p class="form-info">You\'ve signed up, we love you for that! Tell your friends about it.</p></div>'
+				+ '<p class="form-info">You\'ve signed up, we love you for that! Tell your friends about it.</p></div>';
+		
 		if( !email ) {
 			emailInput.val('Please enter your email');			
 		}
@@ -92,6 +97,7 @@ $(function(){
 				$this.hide();
 			/*});*/
 		}		
+		
 		e.preventDefault();	
 	});
 
@@ -120,7 +126,7 @@ $(function(){
 		});
 	});
 
-	
+	/*Fixed menu functionality*/
 	function fixedMenu(){
 		var menuOffset = $('#about').offset().top,
 			nav = $('nav');
